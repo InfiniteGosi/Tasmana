36eec922ceaae3849765b176950033e36976dacc
gosidev <khangho150@gmail.com>
2024-03-01 20:58:57 +0700

    Add project files.

---
+++ Project/FormDangNhap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class FormDangNhap : Form
+    {
+        public FormDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void BT_DangNhap_Click(object sender, EventArgs e)
+        {
+            FormTrangChu f = new FormTrangChu();
+            f.Show();
+            this.Hide();
+        }
+    }
+}
